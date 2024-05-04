465ded800da757de4e31e1045e6243d8eeb54900
Huse Kivrak <huse7@pm.me>
2024-05-04 17:38:59 -0400

    feature - fixed getUserSub's nested data; added more utility queries

--- database/queries.ts
+++ database/queries.ts
@@ -1,9 +1,12 @@
+import {DetailedUser, Vehicle, Subscription} from '@/types/types';
 import {db} from '.';
 import {vehicles, subscriptions, users} from './schema';
 import {eq} from 'drizzle-orm';
 
-export const getUserSubscriptions = async (userId: number) => {
-	return await db
+export const getUserSubscriptions = async (
+	userId: number
+): Promise<Subscription[]> => {
+	const rows = await db
 		.select({
 			subscriptions,
 		})
@@ -11,4 +14,32 @@ export const getUserSubscriptions = async (userId: number) => {
 		.leftJoin(vehicles, eq(subscriptions.vehicleId, vehicles.id))
 		.leftJoin(users, eq(vehicles.userId, users.id))
 		.where(eq(users.id, userId));
+	return rows.map((row) => row.subscriptions);
+};
+
+export const getUserVehicles = async (userId: number): Promise<Vehicle[]> => {
+	const rows = await db
+		.select()
+		.from(vehicles)
+		.where(eq(vehicles.userId, userId));
+	return rows;
+};
+
+export const getDetailedUser = async (
+	userId: number
+): Promise<DetailedUser> => {
+	const rows = await db.select().from(users).where(eq(users.id, userId));
+	if (rows.length === 0) {
+		throw new Error(`No such user (id: ${userId})`);
+	}
+
+	const user = rows[0];
+	const userSubscriptions = await getUserSubscriptions(user.id);
+	const userVehicles = await getUserVehicles(user.id);
+
+	return {
+		...user,
+		vehicles: userVehicles,
+		subscriptions: userSubscriptions,
+	};
 };
