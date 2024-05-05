ea0a1f3affb168e19a55e6715bd27fcc43674d08
Huse Kivrak <huse7@pm.me>
2024-05-04 20:16:16 -0400

    git commit -m ' added utility func for fetching all users+user assets; docstrings'

--- database/queries.ts
+++ database/queries.ts
@@ -3,6 +3,26 @@ import {db} from '.';
 import {vehicles, subscriptions, users} from './schema';
 import {eq} from 'drizzle-orm';
 
+/**
+ * Fetches all vehicles for a given user id.
+ *
+ * @param userId The ID of the user.
+ * @returns An array of all {@link Vehicle} objects for the user.
+ */
+export const getUserVehicles = async (userId: number): Promise<Vehicle[]> => {
+	const rows = await db
+		.select()
+		.from(vehicles)
+		.where(eq(vehicles.userId, userId));
+	return rows;
+};
+
+/**
+ * Fetches all subscriptions for a given user id.
+ *
+ * @param userId The ID of the user.
+ * @returns An array of all {@link Subscription} objects for the user.
+ */
 export const getUserSubscriptions = async (
 	userId: number
 ): Promise<Subscription[]> => {
@@ -17,14 +37,12 @@ export const getUserSubscriptions = async (
 	return rows.map((row) => row.subscriptions);
 };
 
-export const getUserVehicles = async (userId: number): Promise<Vehicle[]> => {
-	const rows = await db
-		.select()
-		.from(vehicles)
-		.where(eq(vehicles.userId, userId));
-	return rows;
-};
+/**
+ * Fetches a user and all associated vehicles and subscriptions.
 
+ * @param userId
+ * @returns A {@link DetailedUser}.
+ */
 export const getDetailedUser = async (
 	userId: number
 ): Promise<DetailedUser> => {
@@ -42,4 +60,36 @@ export const getDetailedUser = async (
 		vehicles: userVehicles,
 		subscriptions: userSubscriptions,
 	};
+};
+
+/**
+ * Calls {@link getDetailedUser} for all users in the database.
+ * @returns  An array of {@link DetailedUser} objects.
+ */
+export const getAllDetailedUsers = async (): Promise<DetailedUser[]> => {
+	const allUsers = await db.select().from(users);
+	const userPromises = allUsers.map((user) => getDetailedUser(user.id));
+	const results = await Promise.allSettled(userPromises);
+
+	const detailedUsers: DetailedUser[] = [];
+	const errors: {userId: number; error: any}[] = [];
+
+	results.forEach((result, index) => {
+		if (result.status === 'fulfilled') {
+			detailedUsers.push(result.value);
+		} else {
+			const userId = allUsers[index].id;
+
+			errors.push({
+				userId,
+				error: result.reason,
+			});
+		}
+	});
+
+	if (errors.length) {
+		console.error('Errors fetching user details:', errors);
+	}
+
+	return detailedUsers;
 };
