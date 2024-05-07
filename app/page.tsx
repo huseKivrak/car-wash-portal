68ba01eea4f2fd0383b7a69855a7f0d897e8a6b8
Huse Kivrak <huse7@pm.me>
2024-05-07 01:51:37 -0400

    refactor - dashboard styling, etc.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-import {redirect} from 'next/navigation';
-export default function App() {
-	redirect('/users');
+import { CustomerDashboard } from '@/components/CustomerDashboard';
+import { getDetailedUser } from '@/database/queries';
+
+export default async function App() {
+	const user = await getDetailedUser(1);
+	return (
+		<div className='flex flex-col items-center justify-center'>
+			<CustomerDashboard user={user} />
+		</div>
+	);
 }
--- components/CustomerDashboard.tsx
+++ components/CustomerDashboard.tsx
@@ -12,11 +12,14 @@ import { SubscriptionCard } from './SubscriptionCard';
 export async function CustomerDashboard({ user }: { user: DetailedUser }) {
 	const subs = user.subscriptions;
 	return (
-		<div className='flex flex-col border border-zinc-200'>
-			<h1>Subscriptions: {subs.length}</h1>
-			{subs.map((sub) => (
-				<SubscriptionCard subscription={sub} key={sub.id} />
-			))}
+		<div className='flex flex-col items-start justify-start'>
+			<h1 className='text-5xl'>{user.fullName}</h1>
+			<div className='flex flex-col border-2 border-zinc-400'>
+				<h2 className='text-2xl'>Subscriptions: {subs.length}</h2>
+				{subs.map((sub) => (
+					<SubscriptionCard subscription={sub} key={sub.id} />
+				))}
+			</div>
 		</div>
 	);
 }
