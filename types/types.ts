fb89841e69de39e65ff1a7d3a518164c4d4ef623
Huse Kivrak <huse7@pm.me>
2024-05-07 14:00:23 -0400

    added zod infered types for washes, purchases

--- types/types.ts
+++ types/types.ts
@@ -6,6 +6,10 @@ import {
 	insertVehicleSchema,
 	selectSubscriptionSchema,
 	insertSubscriptionSchema,
+	selectWashSchema,
+	insertWashSchema,
+	selectPurchaseSchema,
+	insertPurchaseSchema,
 } from '@/database/schema';
 
 export type User = z.infer<typeof selectUserSchema>;
@@ -16,6 +20,12 @@ export type NewVehicle = z.infer<typeof insertVehicleSchema>;
 
 export type NewSubscription = z.infer<typeof insertSubscriptionSchema>;
 export type Subscription = z.infer<typeof selectSubscriptionSchema>;
+
+export type Wash = z.infer<typeof selectWashSchema>;
+export type NewWash = z.infer<typeof insertWashSchema>;
+
+export type Purchase = z.infer<typeof selectPurchaseSchema>;
+export type NewPurchase = z.infer<typeof insertPurchaseSchema>;
 
 export interface DetailedSubscription extends Subscription {
 	vehicle: Vehicle;
