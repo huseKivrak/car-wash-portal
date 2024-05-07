5ecf018e4f3bbb04785cdde30f0f7f77cd85acaa
Huse Kivrak <huse7@pm.me>
2024-05-07 18:35:56 -0400

    minor - set up dynamic route for subscriptions

--- app/subscriptions/[subscriptionId]/page.tsx
+++ app/subscriptions/[subscriptionId]/page.tsx
@@ -1,4 +1,13 @@
-export default function SubscriptionDetailPage() {
+import { db } from '@/database';
+import { subscriptions } from '@/database/schema';
+
+export default async function SubscriptionDetailPage({
+	params,
+}: {
+	params: { subscriptionId: string };
+}) {
+	const { subscriptionId } = params;
+
 	return (
 		<div>
 			<h1>Subscription Detail</h1>
