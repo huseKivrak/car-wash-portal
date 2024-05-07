4fa1815af0b84937e42b6ab55d084c01bfe524e3
Huse Kivrak <huse7@pm.me>
2024-05-06 23:41:25 -0400

    minor - uses vehicle title util

--- components/SubscriptionCard.tsx
+++ components/SubscriptionCard.tsx
@@ -2,28 +2,27 @@ import { DetailedSubscription } from '@/types/types';
 import {
 	Card,
 	CardContent,
-	CardDescription,
 	CardFooter,
 	CardHeader,
 	CardTitle,
 } from '@/components/ui/card';
 import { Badge } from './ui/badge';
-import { getStatusColor } from '@/lib/utils';
-
+import { getStatusColor, makeVehicleTitle } from '@/lib/utils';
 export function SubscriptionCard({
 	subscription,
 }: {
 	subscription: DetailedSubscription;
 }) {
 	const { subscriptionStatus, startDate, endDate } = subscription;
-	const { year, make, model, licensePlate } = subscription.vehicle;
+	const { licensePlate } = subscription.vehicle;
+	const vehicleTitle = makeVehicleTitle(subscription.vehicle);
 
 	const color = getStatusColor(subscriptionStatus);
 	return (
 		<Card>
 			<CardHeader className='flex flex-row items-center justify-between space-y-0 space-x-2 pb-2'>
 				<CardTitle>
-					<div>{`${year} ${make} ${model}`}</div>
+					<div>{vehicleTitle}</div>
 				</CardTitle>
 				<Badge variant='outline' style={{ backgroundColor: color }}>
 					<div className='text-muted-foreground'>
