0384da0f3e9a82f4450110a64783748d086df868
Huse Kivrak <huse7@pm.me>
2024-05-08 15:02:56 -0400

    minor - added 'success' variant for successful form submissions

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -25,20 +25,23 @@ const ToastViewport = React.forwardRef<
 ToastViewport.displayName = ToastPrimitives.Viewport.displayName
 
 const toastVariants = cva(
-  "group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border border-stone-200 p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full dark:border-stone-800",
-  {
-    variants: {
-      variant: {
-        default: "border bg-white text-stone-950 dark:bg-stone-950 dark:text-stone-50",
-        destructive:
-          "destructive group border-red-500 bg-red-500 text-stone-50 dark:border-red-900 dark:bg-red-900 dark:text-stone-50",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  }
-)
+	'group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border border-stone-200 p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full dark:border-stone-800',
+	{
+		variants: {
+			variant: {
+				default:
+					'border bg-white text-stone-950 dark:bg-stone-950 dark:text-stone-50',
+				success:
+					'border bg-green-500 text-white dark:bg-green-800 dark:text-white',
+				destructive:
+					'destructive group border-red-500 bg-red-500 text-stone-50 dark:border-red-900 dark:bg-red-900 dark:text-stone-50',
+			},
+		},
+		defaultVariants: {
+			variant: 'default',
+		},
+	}
+);
 
 const Toast = React.forwardRef<
   React.ElementRef<typeof ToastPrimitives.Root>,
