dd03412a656d367daad9edd4d3cb7a587bac5696
Huse Kivrak <huse7@pm.me>
2024-05-08 15:02:27 -0400

    refactor - all assets ordered by last updated; added subscription-fetching query

--- database/queries.ts
+++ database/queries.ts
@@ -1,22 +1,23 @@
 import { DetailedUser } from '@/types/types';
 import { db } from '.';
-import { subscriptions, purchases } from './schema';
-import { asc, sql } from 'drizzle-orm';
+import { subscriptions, purchases, washes, vehicles } from './schema';
+import { asc, desc, sql } from 'drizzle-orm';
 
 const preparedAllUsers = db.query.users
 	.findMany({
 		with: {
 			subscriptions: {
-				orderBy: [asc(subscriptions.startDate)],
+				orderBy: [desc(subscriptions.startDate)],
 				with: {
+					user: true,
 					vehicle: true,
-					washes: true,
-					purchases: true,
+					washes: { orderBy: [desc(washes.createdAt)] },
+					purchases: { orderBy: [desc(purchases.createdAt)] },
 				},
 			},
-			vehicles: true,
-			washes: true,
-			purchases: { orderBy: [asc(purchases.createdAt)] },
+			vehicles: { orderBy: [desc(vehicles.updatedAt)] },
+			washes: { orderBy: [desc(washes.updatedAt)] },
+			purchases: { orderBy: [desc(purchases.updatedAt)] },
 		},
 	})
 	.prepare('get_all_users_with_relations');
@@ -32,14 +33,16 @@ const preparedUser = db.query.users
 		with: {
 			subscriptions: {
 				with: {
+					user: true,
 					vehicle: true,
-					washes: true,
-					purchases: true,
+					washes: { orderBy: [desc(washes.createdAt)] },
+					purchases: { orderBy: [desc(purchases.updatedAt)] },
 				},
+				orderBy: [desc(subscriptions.updatedAt)],
 			},
-			vehicles: true,
-			washes: true,
-			purchases: { orderBy: [asc(purchases.createdAt)] },
+			vehicles: { orderBy: [desc(vehicles.updatedAt)] },
+			washes: { orderBy: [desc(washes.updatedAt)] },
+			purchases: { orderBy: [desc(purchases.updatedAt)] },
 		},
 	})
 	.prepare('get_user_with_relations');
@@ -49,5 +52,18 @@ export async function getDetailedUserById(
 ): Promise<DetailedUser> {
 	const user = await preparedUser.execute({ id: userId });
 	if (!user) throw new Error(`No such user (id: ${userId})`);
+
 	return user;
+}
+
+export async function getAllSubscriptions() {
+	const subscriptions = await db.query.subscriptions.findMany({
+		with: {
+			user: true,
+			vehicle: true,
+			purchases: { orderBy: [desc(purchases.updatedAt)] },
+			washes: { orderBy: [desc(washes.updatedAt)] },
+		},
+	});
+	return subscriptions;
 }
