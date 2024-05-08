51beb150a514b1198dc249ca5deb40daca10e70b
Huse Kivrak <huse7@pm.me>
2024-05-08 15:07:41 -0400

    refactor - fixed form submission + dialog closing; WIP

--- components/DeleteModal.tsx
+++ components/DeleteModal.tsx
@@ -4,31 +4,57 @@ import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogClose } from '@/components/ui/dialog';
 import { SubmitButton } from './SubmitButton';
 import { DialogProps } from '@radix-ui/react-dialog';
-
+import { toast } from './ui/use-toast';
 export function DeleteModal({
 	id,
-	action,
+	deleteAction,
 	dialogControl,
 }: {
 	id: number;
-	action: (formData: FormData) => Promise<any>;
+	deleteAction: (id: number) => Promise<any> | void;
 	dialogControl: {
 		props: DialogProps;
 		dismiss: () => void;
 	};
 }) {
+	const handleSubmit = async () => {
+		const response = await deleteAction(id);
+		console.log('response:', response);
+		if (response.status === 'success') {
+			toast({
+				variant: 'success',
+				title: 'Vehicle added',
+				description: response.message,
+			});
+		} else {
+			toast({
+				variant: 'destructive',
+				title: 'Something went wrong',
+				description: (
+					<pre className='mt-2 w-[400px] rounded-md bg-red-700'>
+						<p className='text-white'>{response.message}</p>
+					</pre>
+				),
+			});
+		}
+		dialogControl.dismiss();
+	};
+
 	return (
 		<Dialog {...dialogControl.props}>
 			<DialogContent>
-				Are you sure you want to delete this subscription?
-				<form>
-					<input type='hidden' value={id} name='subscription_id' />
-					<SubmitButton formAction={action} pendingText='Deleting...'>
-						Delete
+				Are you sure you want to cancel this?
+				<form
+					//@ts-ignore (expects string or FormData. required here SubmitButton)
+					action={deleteAction}
+					onSubmit={handleSubmit}
+				>
+					<SubmitButton variant='destructive' pendingText='Cancelling...'>
+						Confirm
 					</SubmitButton>
 				</form>
-				<DialogClose asChild>
-					<Button>Close</Button>
+				<DialogClose>
+					<Button variant='secondary'>Close</Button>
 				</DialogClose>
 			</DialogContent>
 		</Dialog>
