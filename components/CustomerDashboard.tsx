e7afac7a5447fbeb9bdfa162a87833a4095aee8d
Huse Kivrak <huse7@pm.me>
2024-05-09 14:33:07 -0400

    debugging - reverting to test db querying

--- components/CustomerDashboard.tsx
+++ components/CustomerDashboard.tsx
@@ -1,36 +1,11 @@
 'use client';
 import { useState } from 'react';
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
-} from '@/components/ui/card';
 import { Button } from './ui/button';
-import {
-	User,
-	DetailedSubscription,
-	Vehicle,
-	Wash,
-	Purchase,
-} from '@/types/types';
+import { DetailedUser } from '@/types/types';
 import { SubscriptionCard } from './SubscriptionCard';
 import { AddVehicleForm } from './forms/AddVehicleForm';
 
-export function CustomerDashboard({
-	user,
-	detailedSubscriptions,
-	vehicles,
-	washes,
-	purchases,
-}: {
-	user: User;
-	detailedSubscriptions: DetailedSubscription[];
-	vehicles: Vehicle[];
-	washes: Wash[];
-	purchases: Purchase[];
-}) {
+export function CustomerDashboard({ user }: { user: DetailedUser }) {
 	const [showVehicleForm, setShowVehicleForm] = useState(false);
 	const handleVehicleFormSuccess = () => {
 		setShowVehicleForm(false);
@@ -45,7 +20,7 @@ export function CustomerDashboard({
 			</div>
 			<div className='flex flex-col'>
 				<h2>Subscriptions</h2>
-				{detailedSubscriptions.map((sub) => (
+				{user.subscriptions.map((sub) => (
 					<SubscriptionCard subscription={sub} />
 				))}
 			</div>
@@ -64,3 +39,5 @@ export function CustomerDashboard({
 		</div>
 	);
 }
+
+
