8d3c2ca834624c4d48acb32905c441cca5dae4e6
Huse Kivrak <huse7@pm.me>
2024-05-09 14:22:55 -0400

    feature - WIP

--- app/subscriptions/page.tsx
+++ app/subscriptions/page.tsx
@@ -1,7 +1,15 @@
-export default function SubscriptionsPage() {
+import { DataTable } from '@/components/ui/data-table';
+import { subscriptionColumns } from '../../components/table/subscriptionColumns';
+import { subscriptions } from '@/database/schema';
+import { db } from '@/database';
+import { getAllSubscriptions } from '@/database/queries';
+
+export default async function SubscriptionsPage() {
+	const subscriptions = await getAllSubscriptions();
 	return (
 		<div>
 			<h1>Subscriptions</h1>
+			<DataTable data={subscriptions} columns={subscriptionColumns} />
 		</div>
 	);
 }
