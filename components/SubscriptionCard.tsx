b0dc038de2c6d18b643986646b25eeb33ca4b998
Huse Kivrak <huse7@pm.me>
2024-05-09 14:22:28 -0400

    refactor - WIP styling

--- components/SubscriptionCard.tsx
+++ components/SubscriptionCard.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import { DetailedSubscription } from '@/types/types';
 import {
 	Card,
@@ -7,39 +10,57 @@ import {
 	CardTitle,
 } from '@/components/ui/card';
 import { Badge } from './ui/badge';
-import { getStatusColor, makeVehicleTitle } from '@/lib/utils';
+import { formatDateTime, getStatusColor, makeVehicleTitle } from '@/lib/utils';
+import { Separator } from './ui/separator';
 export function SubscriptionCard({
 	subscription,
 }: {
 	subscription: DetailedSubscription;
 }) {
-	const { subscriptionStatus, startDate, endDate } = subscription;
-	const { licensePlate } = subscription.vehicle;
-	const vehicleTitle = makeVehicleTitle(subscription.vehicle);
+	const { subscriptionStatus, startDate, endDate, vehicle, washes, purchases } =
+		subscription;
+	const vehicleTitle = makeVehicleTitle(vehicle);
 
 	const color = getStatusColor(subscriptionStatus);
+
 	return (
 		<Card>
-			<CardHeader className='flex flex-row items-center justify-between space-y-0 space-x-2 pb-2'>
-				<CardTitle>
-					<div>{vehicleTitle}</div>
+			<CardHeader className='flex space-y-1 pb-1'>
+				<CardTitle className='flex justify-between'>
+					<div className='text-sm'>{vehicleTitle}</div>
+					<Badge variant='outline' style={{ backgroundColor: color }}>
+						<div className='text-muted-foreground'>
+							{subscriptionStatus.toUpperCase()}
+						</div>
+					</Badge>
 				</CardTitle>
-				<Badge variant='outline' style={{ backgroundColor: color }}>
-					<div className='text-muted-foreground'>
-						{subscriptionStatus.toUpperCase()}
+				<div className='text-sm text-stone-500 dark:text-stone-400'>
+					<p className='font-bold'>
+						Washes Left: {subscription.remainingWashes}
+					</p>
+					<div className='flex items-center'>
+						<p className='font-medium'>Last Wash:</p>
+						<p className='ml-2 font-medium text-sm'>
+							{formatDateTime(washes[0].createdAt)}
+						</p>
 					</div>
-				</Badge>
-			</CardHeader>
-			<CardContent className='w-80 text-sm text-muted-foreground'>
-				<div>Plate: {licensePlate}</div>
-				<div className='font-bold'>
-					Remaining Washes: {subscription.remainingWashes}
+					<p>Expires: {endDate ? formatDateTime(endDate, false) : 'n/a'}</p>
 				</div>
-				<div className=''>Started: {startDate.toLocaleDateString()}</div>
-				<div className='font-semibold'>
-					Expires: {endDate?.toLocaleDateString()}
+				<Separator />
+			</CardHeader>
+			<CardContent className='flex flex-col text-sm'>
+				<div>
+					<h4>All washes:</h4>
+					<ul className='list-disc list-inside text-green-300 '>
+						{washes.map((w) => (
+							<li key={w.id} className='px-2 py-0 font-mono text-xs'>
+								{formatDateTime(w.createdAt)}
+							</li>
+						))}
+					</ul>
 				</div>
 			</CardContent>
+			<CardFooter></CardFooter>
 		</Card>
 	);
 }
