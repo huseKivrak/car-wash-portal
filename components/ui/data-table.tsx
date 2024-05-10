233690b1cda9211d8111917df57c9545298f1c3d
Huse Kivrak <huse7@pm.me>
2024-05-10 07:25:52 -0400

    refactor - reverted to tanstack implementation; issue: custom filterFns for each column as global search

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 import {
 	ColumnDef,
 	ColumnFiltersState,
@@ -10,6 +11,9 @@ import {
 	getPaginationRowModel,
 	getSortedRowModel,
 	useReactTable,
+	FilterFn,
+	Row,
+	Column,
 } from '@tanstack/react-table';
 
 import {
@@ -20,11 +24,14 @@ import {
 	TableHeader,
 	TableRow,
 } from '@/components/ui/table';
-import { Input } from './input';
+
 import { Pagination } from './pagination';
+
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[];
 	data: TData[];
+	filterColumn: string;
+	showPagination: boolean;
 }
 
 export function DataTable<TData, TValue>({
@@ -33,84 +40,139 @@ export function DataTable<TData, TValue>({
 }: DataTableProps<TData, TValue>) {
 	const [sorting, setSorting] = useState<SortingState>([]);
 	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+	const [globalFilter, setGlobalFilter] = useState<string>('');
+
+	const globalFilterFn: FilterFn<any> = (
+		row,
+		columnId,
+		filterValue: string
+	) => {
+		console.log('ROW COLUMN FILTERS:', row.columnFilters);
+		console.log('ROW COLUMN FILTERS @:', row.columnFiltersMeta);
+		const search = filterValue.toLowerCase();
+		let value = row.getValue(columnId) as string;
+		if (typeof value === 'number') value = String(value);
+
+		return value?.toLowerCase().includes(search);
+	};
+
 	const table = useReactTable({
 		data,
 		columns,
+		filterFns: {},
 		getCoreRowModel: getCoreRowModel(),
 		getPaginationRowModel: getPaginationRowModel(),
 		onSortingChange: setSorting,
 		getSortedRowModel: getSortedRowModel(),
 		onColumnFiltersChange: setColumnFilters,
 		getFilteredRowModel: getFilteredRowModel(),
+		onGlobalFilterChange: setGlobalFilter,
+		globalFilterFn: globalFilterFn,
 		state: {
 			sorting,
 			columnFilters,
+			globalFilter,
 		},
 	});
-
 	return (
 		<div>
-			<div className='rounded-md border'>
-				<div className='flex items-center py-4'>
-					<Input
-						placeholder='Filter emails...'
-						value={(table.getColumn('email')?.getFilterValue() as string) ?? ''}
-						onChange={(event) =>
-							table.getColumn('email')?.setFilterValue(event.target.value)
-						}
-						className='max-w-sm'
-					/>
-				</div>
-				<Table>
-					<TableHeader>
-						{table.getHeaderGroups().map((headerGroup) => (
-							<TableRow key={headerGroup.id}>
-								{headerGroup.headers.map((header) => {
-									return (
-										<TableHead key={header.id}>
-											{header.isPlaceholder
-												? null
-												: flexRender(
-														header.column.columnDef.header,
-														header.getContext()
-												  )}
-										</TableHead>
-									);
-								})}
+			<Table>
+				<TableHeader>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map((header) => {
+								return (
+									<TableHead key={header.id}>
+										{header.isPlaceholder
+											? null
+											: flexRender(
+													header.column.columnDef.header,
+													header.getContext()
+											  )}
+
+										{header.column.getCanFilter() ? (
+											<div>
+												<Filter column={header.column} />
+											</div>
+										) : null}
+									</TableHead>
+								);
+							})}
+						</TableRow>
+					))}
+				</TableHeader>
+				<TableBody>
+					{table.getRowModel().rows?.length ? (
+						table.getRowModel().rows.map((row) => (
+							<TableRow
+								key={row.id}
+								data-state={row.getIsSelected() && 'selected'}
+							>
+								{row.getVisibleCells().map((cell) => (
+									<TableCell key={cell.id}>
+										{flexRender(cell.column.columnDef.cell, cell.getContext())}
+									</TableCell>
+								))}
 							</TableRow>
-						))}
-					</TableHeader>
-					<TableBody>
-						{table.getRowModel().rows?.length ? (
-							table.getRowModel().rows.map((row) => (
-								<TableRow
-									key={row.id}
-									data-state={row.getIsSelected() && 'selected'}
-								>
-									{row.getVisibleCells().map((cell) => (
-										<TableCell key={cell.id}>
-											{flexRender(
-												cell.column.columnDef.cell,
-												cell.getContext()
-											)}
-										</TableCell>
-									))}
-								</TableRow>
-							))
-						) : (
-							<TableRow>
-								<TableCell
-									colSpan={columns.length}
-									className='h-24 text-center'
-								>
-									No results.
-								</TableCell>
-							</TableRow>
-						)}
-					</TableBody>
-				</Table>
-			</div>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={columns.length} className='h-24 text-center'>
+								No results.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
 			<Pagination table={table} />
 		</div>
+	);
+}
+
+function Filter({ column }: { column: Column<any, unknown> }) {
+	const columnFilterValue = column.getFilterValue();
+
+	return (
+		<DebouncedInput
+			type='text'
+			value={(columnFilterValue ?? '') as string}
+			onChange={(value) => column.setFilterValue(value)}
+			placeholder={`Search...`}
+			className='w-36 border shadow rounded'
+		/>
+	);
+}
+
+// A typical debounced input react component
+function DebouncedInput({
+	value: initialValue,
+	onChange,
+	debounce = 500,
+	...props
+}: {
+	value: string | number;
+	onChange: (value: string | number) => void;
+	debounce?: number;
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'>) {
+	const [value, setValue] = useState(initialValue);
+
+	useEffect(() => {
+		setValue(initialValue);
+	}, [initialValue]);
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			onChange(value);
+		}, debounce);
+
+		return () => clearTimeout(timeout);
+	}, [value]);
+
+	return (
+		<input
+			{...props}
+			value={value}
+			onChange={(e) => setValue(e.target.value)}
+		/>
 	);
 }
