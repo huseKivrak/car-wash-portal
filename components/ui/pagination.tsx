2224369ffc5991d857ebde3511f9ac8ef08490ac
Huse Kivrak <huse7@pm.me>
2024-05-10 16:25:40 -0400

    file - ui

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -21,10 +21,9 @@ interface DataTablePaginationProps<TData> {
 
 export function Pagination<TData>({ table }: DataTablePaginationProps<TData>) {
 	return (
-		<div className='flex items-center justify-between px-2'>
+		<div className='flex items-center px-2'>
 			<div className='flex-1 text-sm text-muted-foreground'>
-				{table.getFilteredSelectedRowModel().rows.length} of{' '}
-				{table.getFilteredRowModel().rows.length} row(s) selected.
+				{table.getFilteredRowModel().rows.length} row(s).
 			</div>
 			<div className='flex items-center space-x-6 lg:space-x-8'>
 				<div className='flex items-center space-x-2'>
