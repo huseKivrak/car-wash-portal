be26e652a297168f1895e0174f2c6aaf3fd85d85
Huse Kivrak <huse7@pm.me>
2024-05-10 07:27:04 -0400

    minor

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import { CustomerDashboard } from '@/components/CustomerDashboard';
-import { getDetailedUser } from '@/database/queries';
-
 export default async function App() {
-	const user = await getDetailedUser(1);
-	return (
-		<div className='flex flex-col items-center justify-center'>
-			<CustomerDashboard user={user} />
-		</div>
-	);
+	return <div className='flex flex-col items-center justify-center'></div>;
 }
