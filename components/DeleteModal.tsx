1f31fe50093bef1efb51e6ed6dc5afbe0f9db5ce
Huse Kivrak <huse7@pm.me>
2024-05-10 07:20:46 -0400

    styling

--- components/DeleteModal.tsx
+++ components/DeleteModal.tsx
@@ -1,16 +1,22 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogClose } from '@/components/ui/dialog';
+import {
+	Dialog,
+	DialogContent,
+	DialogClose,
+	DialogFooter,
+} from '@/components/ui/dialog';
 import { SubmitButton } from './SubmitButton';
 import { DialogProps } from '@radix-ui/react-dialog';
 import { toast } from './ui/use-toast';
+import { CircleAlert } from 'lucide-react';
 export function DeleteModal({
 	id,
 	deleteAction,
 	dialogControl,
 }: {
-	id: string | number;
+	id: number;
 	deleteAction: (id: number) => Promise<any> | void;
 	dialogControl: {
 		props: DialogProps;
@@ -18,8 +24,7 @@ export function DeleteModal({
 	};
 }) {
 	const handleSubmit = async () => {
-		const userId = typeof id === 'number' ? id : parseInt(id);
-		const response = await deleteAction(userId);
+		const response = await deleteAction(id);
 		console.log('response:', response);
 		if (response.status === 'success') {
 			toast({
@@ -43,22 +48,37 @@ export function DeleteModal({
 
 	return (
 		<Dialog {...dialogControl.props}>
-			<DialogContent>
-				Are you sure you want to cancel this?
-				<form
-					//todo
-					//@ts-ignore
-					action={deleteAction}
-					onSubmit={handleSubmit}
-					className='flex flex-col gap-4'
-				>
-					<SubmitButton variant='destructive' pendingText='Cancelling...'>
-						Confirm
-					</SubmitButton>
-				</form>
-				<DialogClose>
-					<Button variant='secondary'>Close</Button>
-				</DialogClose>
+			<DialogContent className='max-w-md'>
+				<div className='text-lg'>Confirm Cancellation</div>
+				<div className='flex items-center text-red-500'>
+					<CircleAlert className='w-5 h-5 mr-2' />
+					<p className='tracking-wide'>
+						Warning: this action cannot be undone.
+					</p>
+				</div>
+				<div className='flex items-center justify-evenly space-x-2'>
+					<form
+						//todo
+						//@ts-ignore
+						action={deleteAction}
+						onSubmit={handleSubmit}
+					>
+						<SubmitButton
+							variant='destructive'
+							pendingText='Cancelling...'
+							size='lg'
+						>
+							Confirm
+						</SubmitButton>
+					</form>
+					<DialogFooter>
+						<DialogClose asChild>
+							<Button variant='secondary' size='lg'>
+								Close
+							</Button>
+						</DialogClose>
+					</DialogFooter>
+				</div>
 			</DialogContent>
 		</Dialog>
 	);
