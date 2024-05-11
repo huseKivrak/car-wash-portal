190499a05e899ef4c9833e62065e1fed5d574ddd
Huse Kivrak <huse7@pm.me>
2024-05-11 19:17:09 -0400

    refactor - redirects on select without shft+enter

--- components/UsersAutocomplete.tsx
+++ components/UsersAutocomplete.tsx
@@ -1,57 +1,62 @@
-'use client';
+"use client";
 
-import { AutoComplete, type Option } from './ui/autocomplete';
-import { useState, useEffect } from 'react';
-import { DetailedUser } from '@/types/types';
-import Link from 'next/link';
-import { buttonVariants } from './ui/button';
-import { cn } from '@/lib/utils';
-import { useRouter } from 'next/navigation';
-import { ArrowBigDown } from 'lucide-react';
-import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
-import { KeyCombo, Keys, ShortcutsProvider } from '@/lib/keyboardShortcuts';
-export function UsersAutocomplete({ users }: { users: DetailedUser[] }) {
-	const [isLoading, setLoading] = useState(false);
-	const [isDisabled, setDisbled] = useState(false);
-	const [value, setValue] = useState<Option>();
-	const router = useRouter();
+import { AutoComplete, type Option } from "./ui/autocomplete";
+import { useState, useEffect } from "react";
+import { DetailedUser } from "@/types/types";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { KeyCombo, Keys, ShortcutsProvider } from "@/lib/keyboardShortcuts";
+export function UsersAutocomplete({
+  users,
+  className,
+}: {
+  users: DetailedUser[];
+  className?: string;
+}) {
+  console.log("users:", users);
+  const [isLoading, setLoading] = useState(false);
+  const [isDisabled, setDisbled] = useState(false);
+  const [value, setValue] = useState<Option>();
+  const router = useRouter();
 
-	const options: Option[] = users.map((user) => ({
-		value: user.id.toString(),
-		label: user.name,
-		id: user.id.toString(),
-	}));
+  const options: Option[] = users.map((user) => ({
+    value: user.id.toString(),
+    label: user.name,
+    id: user.id.toString(),
+  }));
 
-	useEffect(() => {
-		const handleKeyDown = (event: KeyboardEvent) => {
-			if (
-				event.key === 'Enter' &&
-				// && event.shiftKey
-				value
-			) {
-				router.push(`/users/${value.value}`);
-			}
-		};
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (
+        event.key === "Enter" &&
+        // && event.shiftKey
+        value
+      ) {
+        router.push(`/users/${value.value}`);
+      }
+    };
 
-		window.addEventListener('keydown', handleKeyDown);
-		return () => {
-			window.removeEventListener('keydown', handleKeyDown);
-		};
-	}, [value, router]);
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [value, router]);
 
-	return (
-		<div className='not-prose mt-8 flex gap-4'>
-			<AutoComplete
-				options={options}
-				emptyMessage='No users found.'
-				placeholder='Search by user name...'
-				isLoading={isLoading}
-				onValueChange={setValue}
-				value={value}
-				disabled={isDisabled}
-			/>
-			{value && (
-				<div className='flex items-center gap-2'>
+  return (
+    <div className={cn("not-prose flex w-full rounded-lg p-4", className)}>
+      <AutoComplete
+        options={options}
+        emptyMessage="No users found."
+        placeholder="Search..."
+        isLoading={isLoading}
+        onValueChange={setValue}
+        value={value}
+        disabled={isDisabled}
+      />
+      {/* {value && (
+				<div className='flex items-center gap-4'>
 					<Link
 						href={`/users/${value.value}`}
 						className={cn(buttonVariants({ size: 'sm', variant: 'default' }))}
@@ -64,7 +69,7 @@ export function UsersAutocomplete({ users }: { users: DetailedUser[] }) {
 						</ShortcutsProvider>
 					</div>
 				</div>
-			)}
-		</div>
-	);
+			)} */}
+    </div>
+  );
 }
