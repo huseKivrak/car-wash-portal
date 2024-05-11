3ed4593fb0f1d31f998a337a7f92c4a98a6582c6
Huse Kivrak <huse7@pm.me>
2024-05-11 19:09:38 -0400

    fix - max connections/timeout errors

--- database/index.ts
+++ database/index.ts
@@ -1,10 +1,25 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import { connectionString } from '@/drizzle.config';
-import * as schema from './schema';
-if (!connectionString) throw new Error('DATABASE_URL not found in .env.local');
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { connectionString } from "@/drizzle.config";
+import * as schema from "./schema";
 
-const client = postgres(connectionString, {
-	prepare: false,
-});
-export const db = drizzle(client, { schema, logger: true });
+let connection: postgres.Sql;
+if (process.env.NODE_ENV === "production") {
+  connection = postgres(connectionString!, { prepare: false });
+} else {
+  const globalConnection = global as typeof globalThis & {
+    connection: postgres.Sql;
+  };
+
+  if (!globalConnection.connection) {
+    globalConnection.connection = postgres(connectionString!, {
+      prepare: false,
+    });
+  }
+
+  connection = globalConnection.connection;
+}
+
+const db = drizzle(connection, { schema, logger: false });
+export * from "./schema";
+export { db };
