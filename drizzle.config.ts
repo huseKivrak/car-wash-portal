16283118aa94a93b28652ddc76935ebd8b17f034
Huse Kivrak <huse7@pm.me>
2024-05-11 19:07:33 -0400

    fixing max connection/timeout

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,19 @@
-import type { Config } from 'drizzle-kit';
-import { cwd } from 'process';
-import { loadEnvConfig } from '@next/env';
+import type { Config } from "drizzle-kit";
+import { cwd } from "process";
+import { loadEnvConfig } from "@next/env";
 
 loadEnvConfig(cwd());
-export const connectionString = process.env.DATABASE_URL;
+export const connectionString =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL
+    : process.env.LOCAL_DATABASE;
 
 export default {
-	schema: './database/schema.ts',
-	driver: 'pg',
-	out: './database/migrations',
-	dbCredentials: {
-		connectionString: connectionString!,
-	},
-	verbose: true,
+  schema: "./database/schema.ts",
+  driver: "pg",
+  out: "./database/migrations",
+  dbCredentials: {
+    connectionString: connectionString!,
+  },
+  verbose: true,
 } satisfies Config;
