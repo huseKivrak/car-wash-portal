497107a6f900e8541c79dd7fb9509796bc0ef9a3
Huse Kivrak <huse7@pm.me>
2024-05-12 16:38:19 -0400

    feature: added utility action for updating user's updatedAt; fixed updateSubscription; added utility func to all subscription actions

--- actions/subscriptions.ts
+++ actions/subscriptions.ts
@@ -1,13 +1,12 @@
-'use server';
+"use server";
 
-import { db } from '@/database';
-import { subscriptions, insertSubscriptionSchema } from '@/database/schema';
-import { eq } from 'drizzle-orm';
-import { z, ZodError } from 'zod';
-
-import { revalidatePath } from 'next/cache';
-import { NewSubscription, Subscription } from '@/types/types';
-
+import { db } from "@/database";
+import { subscriptions, insertSubscriptionSchema } from "@/database/schema";
+import { eq } from "drizzle-orm";
+import { z, ZodError } from "zod";
+import { revalidatePath } from "next/cache";
+import { NewSubscription, Subscription } from "@/types/types";
+import { updateUserUpdatedAt } from "./users";
 /**
  *
  * @param formInputs
@@ -15,64 +14,66 @@ import { NewSubscription, Subscription } from '@/types/types';
  */
 type SubscriptionFields = z.infer<typeof insertSubscriptionSchema>;
 export async function addSubscription(formInputs: SubscriptionFields) {
-	let newSubscription;
-	try {
-		const values = insertSubscriptionSchema.parse(formInputs);
+  let newSubscription;
+  try {
+    const values = insertSubscriptionSchema.parse(formInputs);
 
-		const result = await db.insert(subscriptions).values(values).returning();
-		newSubscription = result[0];
-	} catch (error) {
-		if (error instanceof ZodError) {
-			console.error('Zod issues:', error.issues);
-			return {
-				status: 'validation error',
-				errors: error.issues.map((issue) => ({
-					path: issue.path,
-					message: issue.message,
-				})),
-			};
-		} else {
-			return {
-				status: 'server error',
-				message: `An error occured while adding subscription: ${error}`,
-			};
-		}
-	}
-	revalidatePath('/', 'layout');
-	return {
-		status: 'success',
-		message: `Subscription ${newSubscription.id} created`,
-	};
+    const result = await db.insert(subscriptions).values(values).returning();
+    newSubscription = result[0];
+    await updateUserUpdatedAt(newSubscription.userId);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      console.error("Zod issues:", error.issues);
+      return {
+        status: "validation error",
+        errors: error.issues.map((issue) => ({
+          path: issue.path,
+          message: issue.message,
+        })),
+      };
+    } else {
+      return {
+        status: "server error",
+        message: `An error occured while adding subscription: ${error}`,
+      };
+    }
+  }
+  revalidatePath("/", "layout");
+  return {
+    status: "success",
+    message: `Subscription ${newSubscription.id} created`,
+  };
 }
 
 export async function cancelSubscription(subscriptionId: number) {
-	let cancelledSubscription: Subscription;
-	try {
-		const currentDate = new Date();
+  let cancelledSubscription: Subscription;
+  try {
+    const currentDate = new Date();
 
-		const result = await db
-			.update(subscriptions)
-			.set({
-				isCancelled: true,
-				cancelledAt: currentDate,
-				updatedAt: currentDate,
-				subscriptionStatus: 'cancelled',
-			})
-			.where(eq(subscriptions.id, subscriptionId))
-			.returning();
-		cancelledSubscription = result[0];
-	} catch (error) {
-		return {
-			status: 'server error',
-			message: `An error occured while cancelling subscription: ${error}`,
-		};
-	}
+    const result = await db
+      .update(subscriptions)
+      .set({
+        isCancelled: true,
+        cancelledAt: currentDate,
+        updatedAt: currentDate,
+        subscriptionStatus: "cancelled",
+      })
+      .where(eq(subscriptions.id, subscriptionId))
+      .returning();
+    cancelledSubscription = result[0];
+    await updateUserUpdatedAt(cancelledSubscription.userId);
+  } catch (error) {
+    return {
+      status: "server error",
+      message: `An error occured while cancelling subscription: ${error}`,
+    };
+  }
 
-	revalidatePath('/', 'layout');
-	return {
-		status: 'success',
-		message: `Subscription #${cancelledSubscription.id} cancelled successfully.`,
-	};
+  revalidatePath("/", "layout");
+  return {
+    status: "success",
+    message: `Subscription #${cancelledSubscription.id} cancelled successfully.`,
+  };
 }
 
 /**
@@ -82,54 +83,59 @@ export async function cancelSubscription(subscriptionId: number) {
  * @returns
  */
 export async function transferSubscription(
-	subscriptionId: number,
-	vehicleId: number
+  subscriptionId: number,
+  vehicleId: number,
 ) {
-	//get current subscription
-	const currentSubscription = await db.query.subscriptions.findFirst({
-		columns: {
-			id: false, //ignore id to prevent duplicates below
-		},
-		where: eq(subscriptions.id, subscriptionId),
-	});
-	if (!currentSubscription) {
-		return { status: 'error', message: 'Original subscription not found.' };
-	} else if (currentSubscription.vehicleId === vehicleId) {
-		return {
-			status: 'error',
-			message: 'This subscription is already applied to this vehicle.',
-		};
-	}
-	const currentDate = new Date();
+  //get current subscription
+  const currentSubscription = await db.query.subscriptions.findFirst({
+    columns: {
+      id: false, //ignore id to prevent duplicates below
+    },
+    where: eq(subscriptions.id, subscriptionId),
+  });
+  if (!currentSubscription) {
+    return { status: "error", message: "Original subscription not found." };
+  } else if (currentSubscription.vehicleId === vehicleId) {
+    return {
+      status: "error",
+      message: "This subscription is already applied to this vehicle.",
+    };
+  }
+  const currentDate = new Date();
 
-	//create new subscription entry with new vehicle Id
-	const newSubscriptionData: NewSubscription = {
-		...currentSubscription,
-		vehicleId,
-	};
+  //create new subscription entry with new vehicle Id
+  const newSubscriptionData: NewSubscription = {
+    ...currentSubscription,
+    subscriptionStatus: "active",
+    isTransferred: false,
+    updatedAt: currentDate,
+    vehicleId,
+  };
 
-	const newSubscriptionResult = await db
-		.insert(subscriptions)
-		.values(newSubscriptionData)
-		.returning();
-	const newSubscription = newSubscriptionResult[0];
+  const newSubscriptionResult = await db
+    .insert(subscriptions)
+    .values(newSubscriptionData)
+    .returning();
+  const newSubscription = newSubscriptionResult[0];
 
-	//Update old subscription to 'transferred'
-	const transferredSubscription = await db
-		.update(subscriptions)
-		.set({
-			subscriptionStatus: 'transferred',
-			isTransferred: true,
-			transferredAt: currentDate,
-			transferredSubscriptionId: newSubscription.id,
-			updatedAt: currentDate,
-		})
-		.returning(); //todo
+  //Update old subscription to 'transferred'
+  const transferredSubscription = await db
+    .update(subscriptions)
+    .set({
+      subscriptionStatus: "transferred",
+      isTransferred: true,
+      transferredAt: currentDate,
+      transferredSubscriptionId: newSubscription.id,
+      updatedAt: currentDate,
+    })
+    .where(eq(subscriptions.id, subscriptionId))
+    .returning();
+  await updateUserUpdatedAt(newSubscription.userId);
 
-	revalidatePath('/');
+  revalidatePath("/");
 
-	return {
-		status: 'success',
-		message: `Subscription transfered to Vehicle #${vehicleId}. New Id: ${newSubscription.id}.`,
-	};
+  return {
+    status: "success",
+    message: `Subscription transfered to Vehicle #${vehicleId}. New Id: ${newSubscription.id}.`,
+  };
 }
--- actions/users.ts
+++ actions/users.ts
@@ -1,78 +1,90 @@
-'use server';
+"use server";
 
-import { db } from '@/database';
-import { users } from '@/database/schema';
-import { eq } from 'drizzle-orm';
-import { z, ZodError } from 'zod';
-import { insertUserSchema } from '@/database/schema';
-import { revalidatePath } from 'next/cache';
-import { User } from '@/types/types';
+import { db } from "@/database";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
+import { z, ZodError } from "zod";
+import { insertUserSchema } from "@/database/schema";
+import { revalidatePath } from "next/cache";
+import { User } from "@/types/types";
 
 type InsertUser = z.infer<typeof insertUserSchema>;
 export async function updateUser(fieldsToUpdate: InsertUser) {
-	const { id, name, phone, email } = insertUserSchema.parse(fieldsToUpdate);
+  const { id, name, phone, email } = insertUserSchema.parse(fieldsToUpdate);
 
-	let updatedUser;
-	try {
-		const updatedRows = await db
-			.update(users)
-			.set({ name, phone, email })
-			.where(eq(users.id, id!))
-			.returning();
+  let updatedUser;
+  try {
+    const updatedRows = await db
+      .update(users)
+      .set({ name, phone, email, updatedAt: new Date() })
+      .where(eq(users.id, id!))
+      .returning();
 
-		updatedUser = updatedRows[0];
-	} catch (error) {
-		if (error instanceof ZodError) {
-			console.error('Zod issues:', error.issues);
-			return {
-				status: 'validation error',
-				errors: error.issues.map((issue) => ({
-					path: issue.path,
-					message: issue.message,
-				})),
-			};
-		} else {
-			return {
-				status: 'server error',
-				message: `An error occured while updating: ${error}`,
-			};
-		}
-	}
-	revalidatePath('/', 'layout');
-	return {
-		status: 'success',
-		message: `${updatedUser.name} has been updated.`,
-	};
+    updatedUser = updatedRows[0];
+  } catch (error) {
+    if (error instanceof ZodError) {
+      console.error("Zod issues:", error.issues);
+      return {
+        status: "validation error",
+        errors: error.issues.map((issue) => ({
+          path: issue.path,
+          message: issue.message,
+        })),
+      };
+    } else {
+      return {
+        status: "server error",
+        message: `An error occured while updating: ${error}`,
+      };
+    }
+  }
+  revalidatePath("/", "layout");
+  return {
+    status: "success",
+    message: `${updatedUser.name} has been updated.`,
+  };
 }
 
 export async function cancelUser(userId: number) {
-	let cancelledUser;
+  let cancelledUser;
 
-	try {
-		const currentDate = new Date();
+  try {
+    const currentDate = new Date();
 
-		const result = await db
-			.update(users)
-			.set({
-				cancelledAt: currentDate,
-				isCancelled: true,
-				updatedAt: currentDate,
-			})
-			.where(eq(users.id, userId))
-			.returning();
-		cancelledUser = result[0];
-		console.log('User account cancelled.');
-	} catch (error) {
-		console.error(error);
-		return {
-			status: 'error',
-			message: `An error occured while cancelling user: ${error} `,
-		};
-	}
+    const result = await db
+      .update(users)
+      .set({
+        cancelledAt: currentDate,
+        isCancelled: true,
+        updatedAt: currentDate,
+      })
+      .where(eq(users.id, userId))
+      .returning();
+    cancelledUser = result[0];
+    console.log("User account cancelled.");
+  } catch (error) {
+    console.error(error);
+    return {
+      status: "error",
+      message: `An error occured while cancelling user: ${error} `,
+    };
+  }
 
-	revalidatePath('/', 'layout');
-	return {
-		status: 'success',
-		message: `${cancelledUser.name}'s account has been cancelled.`,
-	};
+  revalidatePath("/", "layout");
+  return {
+    status: "success",
+    message: `${cancelledUser.name}'s account has been cancelled.`,
+  };
 }
+
+//Utility function to update user updatedAt field
+export const updateUserUpdatedAt = async (userId: number) => {
+  try {
+    await db
+      .update(users)
+      .set({ updatedAt: new Date() })
+      .where(eq(users.id, userId));
+  } catch (error) {
+    console.error("Error updating user updatedAt:", error);
+  }
+};
