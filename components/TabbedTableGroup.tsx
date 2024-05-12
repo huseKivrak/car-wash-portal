216155ac54e504866d2e660db24d9b87a7079b55
Huse Kivrak <huse7@pm.me>
2024-05-12 16:48:10 -0400

    refactor - accepts optional params to show each form ( prevents users table in CustomerDashboard)

--- components/TabbedTableGroup.tsx
+++ components/TabbedTableGroup.tsx
@@ -1,37 +1,55 @@
-import { DataTable } from './table/data-table';
-import { userColumns } from './table/userColumns';
-import { subscriptionColumns } from './table/subscriptionColumns';
-import { purchaseColumns } from './table/purchaseColumns';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { DetailedUser } from '@/types/types';
-import { dummyData } from '@/dummyData';
+import { DataTable } from "./table/data-table";
+import { userColumns } from "./table/userColumns";
+import { subscriptionColumns } from "./table/subscriptionColumns";
+import { purchaseColumns } from "./table/purchaseColumns";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { DetailedUser } from "@/types/types";
+import { dummyData } from "@/dummyData";
+import { Separator } from "./ui/separator";
 
 export function TabbedTableGroup({
-	users = dummyData as unknown as DetailedUser[],
+  users = dummyData as unknown as DetailedUser[],
+  userTable = true,
+  subscriptionTable = true,
+  purchaseTable = true,
 }: {
-	users: DetailedUser[];
+  users: DetailedUser[];
+  userTable?: boolean;
+  subscriptionTable?: boolean;
+  purchaseTable?: boolean;
 }) {
-	const subscriptions = users.map((user) => user.subscriptions).flat();
-	const purchases = users.map((user) => user.purchases).flat();
-	return (
-		<Tabs
-			defaultValue='subscriptions'
-			className='w-full border rounded-lg border-primary'
-		>
-			<TabsList className='grid w-1/3 grid-cols-3 rounded-lg border-primary'>
-				<TabsTrigger value='users'>Users</TabsTrigger>
-				<TabsTrigger value='subscriptions'>Subscriptions</TabsTrigger>
-				<TabsTrigger value='purchases'>Purchases</TabsTrigger>
-			</TabsList>
-			<TabsContent value='users'>
-				<DataTable columns={userColumns} data={users} />
-			</TabsContent>
-			<TabsContent value='subscriptions'>
-				<DataTable columns={subscriptionColumns} data={subscriptions} />
-			</TabsContent>
-			<TabsContent value='purchases'>
-				<DataTable columns={purchaseColumns} data={purchases} />
-			</TabsContent>
-		</Tabs>
-	);
+  const subscriptions = users.map((user) => user.subscriptions).flat();
+  const purchases = users.map((user) => user.purchases).flat();
+  return (
+    <Tabs
+      defaultValue={userTable ? "users" : "subscriptions"}
+      className="h-fit w-full rounded-lg border p-2"
+    >
+      <TabsList className="flex w-fit content-between justify-start" loop>
+        {userTable && <TabsTrigger value="users">Users</TabsTrigger>}
+        {subscriptionTable && (
+          <TabsTrigger value="subscriptions">Subscriptions</TabsTrigger>
+        )}
+        {purchaseTable && (
+          <TabsTrigger value="purchases">Purchases</TabsTrigger>
+        )}
+      </TabsList>
+      <Separator className="my-2" />
+      {userTable && (
+        <TabsContent value="users">
+          <DataTable columns={userColumns} data={users} />
+        </TabsContent>
+      )}
+      {subscriptionTable && (
+        <TabsContent value="subscriptions">
+          <DataTable columns={subscriptionColumns} data={subscriptions} />
+        </TabsContent>
+      )}
+      {purchaseTable && (
+        <TabsContent value="purchases">
+          <DataTable columns={purchaseColumns} data={purchases} />
+        </TabsContent>
+      )}
+    </Tabs>
+  );
 }
