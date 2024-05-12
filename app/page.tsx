7ff74101851046f130e4ab7c62e6bd0ee07bd271
Huse Kivrak <huse7@pm.me>
2024-05-12 16:47:04 -0400

    refactor - shows tables instead of search

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ export default async function App() {
   const users = await getAllDetailedUsers();
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col space-y-2">
       <h1 className="text-4xl font-extrabold tracking-tight text-primary lg:text-5xl">
         AMP Customer Service Portal
       </h1>
-      <UsersAutocomplete users={users} />
+      <TabbedTableGroup users={users} />
     </div>
   );
 }
