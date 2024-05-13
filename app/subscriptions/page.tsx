63c4838aa6cb7407261c1680813a41baeaa14b5e
Huse Kivrak <huse7@pm.me>
2024-05-13 11:51:57 -0400

    debugging

--- app/subscriptions/page.tsx
+++ app/subscriptions/page.tsx
@@ -1,17 +1,16 @@
-import { DataTable } from '@/components/table/data-table';
-import { subscriptionColumns } from '../../components/table/subscriptionColumns';
-import { subscriptions } from '@/database/schema';
-import { db } from '@/database';
-import { getAllSubscriptions } from '@/database/queries';
+import { DataTable } from "@/components/table/data-table";
+import { subscriptionColumns } from "../../components/table/subscriptionColumns";
+
+import { getAllSubscriptions } from "@/database/queries";
 
 export default async function SubscriptionsPage() {
-	const subscriptions = await getAllSubscriptions();
-	return (
-		<div>
-			<h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-primary'>
-				All User Subscriptions
-			</h1>
-			<DataTable data={subscriptions} columns={subscriptionColumns} />
-		</div>
-	);
+  const subscriptions = await getAllSubscriptions();
+  return (
+    <div>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-primary lg:text-5xl">
+        All User Subscriptions
+      </h1>
+      <DataTable data={subscriptions} columns={subscriptionColumns} />
+    </div>
+  );
 }
