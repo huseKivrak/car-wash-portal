47d5fca7b2f21e25f426b9e36bc2d6d2cffc386b
Huse Kivrak <huse7@pm.me>
2024-05-13 11:39:34 -0400

    debugging

--- database/index.ts
+++ database/index.ts
@@ -3,22 +3,19 @@ import postgres from "postgres";
 import { connectionString } from "@/drizzle.config";
 import * as schema from "./schema";
 
-let connection: postgres.Sql;
-if (process.env.NODE_ENV === "production") {
-  connection = postgres(connectionString!, { prepare: false });
-} else {
-  const globalConnection = global as typeof globalThis & {
-    connection: postgres.Sql;
-  };
+let connection = postgres(connectionString!, { prepare: false });
 
-  if (!globalConnection.connection) {
-    globalConnection.connection = postgres(connectionString!, {
-      prepare: false,
-    });
-  }
+const globalConnection = global as typeof globalThis & {
+  connection: postgres.Sql;
+};
 
-  connection = globalConnection.connection;
+if (!globalConnection.connection) {
+  globalConnection.connection = postgres(connectionString!, {
+    prepare: false,
+  });
 }
+
+connection = globalConnection.connection;
 
 const db = drizzle(connection, { schema, logger: false });
 export * from "./schema";
