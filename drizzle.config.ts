0f7a920170d558b203bfe11a9c0ccf15baca5c84
Huse Kivrak <huse7@pm.me>
2024-05-31 00:54:25 -0400

    fix - no longer used

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,16 @@
-import type { Config } from "drizzle-kit";
-import { cwd } from "process";
-import { loadEnvConfig } from "@next/env";
+// import type { Config } from "drizzle-kit";
+// import { cwd } from "process";
+// import { loadEnvConfig } from "@next/env";
 
-loadEnvConfig(cwd());
-export const connectionString = process.env.DATABASE_URL;
+// loadEnvConfig(cwd());
+// export const connectionString = process.env.DATABASE_URL;
 
-export default {
-  schema: "./database/schema.ts",
-  driver: "pg",
-  out: "./database/migrations",
-  dbCredentials: {
-    connectionString: connectionString!,
-  },
-  verbose: true,
-} satisfies Config;
+// export default {
+//   schema: "./database/schema.ts",
+//   driver: "pg",
+//   out: "./database/migrations",
+//   dbCredentials: {
+//     connectionString: connectionString!,
+//   },
+//   verbose: true,
+// } satisfies Config;
