a20d6b11b4fe7d94d33d71311a20409aa6780d60
Huse Kivrak <huse7@pm.me>
2024-06-01 19:23:26 -0400

    reorganized and commented for clarity

--- database/queries.ts
+++ database/queries.ts
@@ -3,65 +3,6 @@ import { db } from ".";
 import { subscriptions, purchases, washes, vehicles } from "./schema";
 import { desc, sql } from "drizzle-orm";
 
-const preparedAllUsers = db.query.users
-  .findMany({
-    with: {
-      subscriptions: {
-        orderBy: [desc(subscriptions.startDate)],
-        with: {
-          user: true,
-          vehicle: true,
-          washes: { orderBy: [desc(washes.createdAt)] },
-          purchases: { orderBy: [desc(purchases.createdAt)] },
-        },
-      },
-      vehicles: { orderBy: [desc(vehicles.updatedAt)] },
-      washes: { orderBy: [desc(washes.updatedAt)] },
-      purchases: { orderBy: [desc(purchases.updatedAt)] },
-    },
-  })
-  .prepare("get_all_users_with_relations");
-
-export async function getAllDetailedUsersPrepared(): Promise<DetailedUser[]> {
-  const allUsers = await preparedAllUsers.execute();
-  return allUsers;
-}
-
-const preparedUser = db.query.users
-  .findFirst({
-    where: (users, { eq }) => eq(users.id, sql.placeholder("id")),
-    with: {
-      subscriptions: {
-        with: {
-          user: true,
-          vehicle: true,
-          washes: { orderBy: [desc(washes.createdAt)] },
-          purchases: { orderBy: [desc(purchases.updatedAt)] },
-        },
-        orderBy: [desc(subscriptions.updatedAt)],
-      },
-      vehicles: { orderBy: [desc(vehicles.updatedAt)] },
-      washes: { orderBy: [desc(washes.updatedAt)] },
-      purchases: { orderBy: [desc(purchases.updatedAt)] },
-    },
-  })
-  .prepare("get_user_with_relations");
-
-export async function getDetailedUserByIdPrepared(
-  userId: number,
-): Promise<DetailedUser> {
-  const user = await preparedUser.execute({ id: userId });
-  if (!user) throw new Error(`No such user (id: ${userId})`);
-
-  return user;
-}
-
-//////////////////////////////////////////////
-/**
- * Without prepared statements (unsupported on Supabase)
- */
-//////////////////////////////////////////////
-
 export const getAllDetailedUsers = async (): Promise<DetailedUser[]> => {
   const users = await db.query.users.findMany({
     with: {
@@ -117,4 +58,62 @@ export async function getAllSubscriptions() {
     },
   });
   return subscriptions;
+}
+
+/**
+ * Prepared statements are for local development/testing only,
+ * as they're not yet supported by Supabase's Transaction pooler.
+ */
+
+const preparedAllUsers = db.query.users
+  .findMany({
+    with: {
+      subscriptions: {
+        orderBy: [desc(subscriptions.startDate)],
+        with: {
+          user: true,
+          vehicle: true,
+          washes: { orderBy: [desc(washes.createdAt)] },
+          purchases: { orderBy: [desc(purchases.createdAt)] },
+        },
+      },
+      vehicles: { orderBy: [desc(vehicles.updatedAt)] },
+      washes: { orderBy: [desc(washes.updatedAt)] },
+      purchases: { orderBy: [desc(purchases.updatedAt)] },
+    },
+  })
+  .prepare("get_all_users_with_relations");
+
+export async function getAllDetailedUsersPrepared(): Promise<DetailedUser[]> {
+  const allUsers = await preparedAllUsers.execute();
+  return allUsers;
+}
+
+const preparedUser = db.query.users
+  .findFirst({
+    where: (users, { eq }) => eq(users.id, sql.placeholder("id")),
+    with: {
+      subscriptions: {
+        with: {
+          user: true,
+          vehicle: true,
+          washes: { orderBy: [desc(washes.createdAt)] },
+          purchases: { orderBy: [desc(purchases.updatedAt)] },
+        },
+        orderBy: [desc(subscriptions.updatedAt)],
+      },
+      vehicles: { orderBy: [desc(vehicles.updatedAt)] },
+      washes: { orderBy: [desc(washes.updatedAt)] },
+      purchases: { orderBy: [desc(purchases.updatedAt)] },
+    },
+  })
+  .prepare("get_user_with_relations");
+
+export async function getDetailedUserByIdPrepared(
+  userId: number,
+): Promise<DetailedUser> {
+  const user = await preparedUser.execute({ id: userId });
+  if (!user) throw new Error(`No such user (id: ${userId})`);
+
+  return user;
 }
